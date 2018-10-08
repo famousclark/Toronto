2e36690acb12136036f6ae0f9c245b1179d310dc
famousclark <clark.famous@gmail.com>
2018-10-08 02:12:20 -0400

    Add css

--- diarra-hope-priya.php
+++ diarra-hope-priya.php
@@ -12,63 +12,67 @@
       include('./inc/app-nav.php');
     ?>
 
-    <section>
+    <div class="ui container">
 
-      <h2>About Them</h2>
+      <section>
 
-      <!-- Diarra -->
-      <p>I was born in Rochester, and I’ve lived here my whole life. I live in Brighton with my parents and my sister, who is in her senior year of high school. I enjoy living in Rochester however, when I graduate, I’d like to go somewhere new. I’m not exactly sure where I want to go to grad school yet, but I’ll figure it out… eventually. In the meantime I’d really like to visit Europe, Australia, Africa, South America… there’s so many places I haven’t been yet. I just need to find the time (and money) to do it.</p>
+        <h2>About Them</h2>
 
-      <!-- Priya -->
-      <p>My interests include music, art, design and computer science. Initially, I wasn’t sure how I might be able to combine that combination of interests, but I found that the field of mixed reality technology combined those aspects very neatly. It involved both the creative side and technical side of my interests. The environment is designed using principles of good design, and it also involves programming the software to make it work. It is a relatively new industry so there is a lot that is still being discovered and there is lots of research going on in this area, both in academia and the industry.</p>
+        <!-- Diarra -->
+        <p>I was born in Rochester, and I’ve lived here my whole life. I live in Brighton with my parents and my sister, who is in her senior year of high school. I enjoy living in Rochester however, when I graduate, I’d like to go somewhere new. I’m not exactly sure where I want to go to grad school yet, but I’ll figure it out… eventually. In the meantime I’d really like to visit Europe, Australia, Africa, South America… there’s so many places I haven’t been yet. I just need to find the time (and money) to do it.</p>
 
-      <!-- Hope -->
-      <p>I have had a variety of internships and jobs throughout my life. My first job was as a gymnastics coach at Westchester Gymnastics, where I taught to beginners. I spent the summer of 2016 working as the gymnastics coordinator for a day camp run by the Jewish Community Center of Tarrytown, where I worked with over 300 children every day teaching basic gymnastics skills. In the summer of 2017 following my Freshmen year of college, I worked in the User Experience Design department of Mastercard in NYC, and got the chance to work on a variety of exciting projects such as the redesign of MC's website, apps and conduct multiple research tests. This past summer, I was a graphic design intern for Regeneron, a biotechnology company located in Tarrytown NY. In this position, I helped run and manage many of their social media campaigns, create designs for corporate materials, and learned a great deal about marketing within the heathcare industry.
-	    </p>
+        <!-- Priya -->
+        <p>My interests include music, art, design and computer science. Initially, I wasn’t sure how I might be able to combine that combination of interests, but I found that the field of mixed reality technology combined those aspects very neatly. It involved both the creative side and technical side of my interests. The environment is designed using principles of good design, and it also involves programming the software to make it work. It is a relatively new industry so there is a lot that is still being discovered and there is lots of research going on in this area, both in academia and the industry.</p>
 
-    </section>
+        <!-- Hope -->
+        <p>I have had a variety of internships and jobs throughout my life. My first job was as a gymnastics coach at Westchester Gymnastics, where I taught to beginners. I spent the summer of 2016 working as the gymnastics coordinator for a day camp run by the Jewish Community Center of Tarrytown, where I worked with over 300 children every day teaching basic gymnastics skills. In the summer of 2017 following my Freshmen year of college, I worked in the User Experience Design department of Mastercard in NYC, and got the chance to work on a variety of exciting projects such as the redesign of MC's website, apps and conduct multiple research tests. This past summer, I was a graphic design intern for Regeneron, a biotechnology company located in Tarrytown NY. In this position, I helped run and manage many of their social media campaigns, create designs for corporate materials, and learned a great deal about marketing within the heathcare industry.
+  	    </p>
 
-    <section>
+      </section>
 
-      <h2>For the Science!</h2>
+      <section>
 
-      <h3>Work</h3>
+        <h2>For the Science!</h2>
 
-      <!-- Priya -->
-      <p>This past summer I was part of a field school in Ghana which focused on the surveying, reconstruction and structural analysis of Elmina castle and a few other forts along the coast of Ghana. I was able to learn how to use technology such as photogrammetry, which involves taking pictures of an object from multiple angles and then reconstructing it using software. The course also involved the use of technology such as laser scanning and 3D reconstruction. It was very educational for me and helped me narrow down what kind of career path I want to pursue. I am not entirely certain, but I would like to work in a research environment exploring how digital technologies for mixed reality can be combined with the humanities.</p>
+        <h3>Work</h3>
 
-      <!-- Hope -->
-      <p>At University of Rochester, I have taken many classes to help advance my skillsets and knowledge. I have completed courses such as DMS 101, DMS 102, DMS 103, and DMS 104 to ensure that I am prepared for upper level DMS classes. Additionally, I am one course away from completion of my computer science minor, and now have HTML, Python and Java skills. I am currently enrolled in many Business courses, such as Finance, and have previously taken multiple Economics and accounting classes.</p>
+        <!-- Priya -->
+        <p>This past summer I was part of a field school in Ghana which focused on the surveying, reconstruction and structural analysis of Elmina castle and a few other forts along the coast of Ghana. I was able to learn how to use technology such as photogrammetry, which involves taking pictures of an object from multiple angles and then reconstructing it using software. The course also involved the use of technology such as laser scanning and 3D reconstruction. It was very educational for me and helped me narrow down what kind of career path I want to pursue. I am not entirely certain, but I would like to work in a research environment exploring how digital technologies for mixed reality can be combined with the humanities.</p>
 
-    </section>
+        <!-- Hope -->
+        <p>At University of Rochester, I have taken many classes to help advance my skillsets and knowledge. I have completed courses such as DMS 101, DMS 102, DMS 103, and DMS 104 to ensure that I am prepared for upper level DMS classes. Additionally, I am one course away from completion of my computer science minor, and now have HTML, Python and Java skills. I am currently enrolled in many Business courses, such as Finance, and have previously taken multiple Economics and accounting classes.</p>
 
-    <section>
+      </section>
 
-      <h2>For the Humanities</h2>
+      <section>
 
-      <h3>Music</h3>
+        <h2>For the Humanities</h2>
 
-      <!-- Diarra -->
-       <p>In my free time I enjoy playing the piano. I have been playing classical piano since I was 4 years old, and it’s something that I’ve always enjoyed. I wouldn’t say that I’m the next Mozart or anything though! One of my favorite composers is Debussy- I really like romantic-era music. I took several music theory classes when I was in high school, but music theory doesn’t really appeal to me all that much.</p>
+        <h3>Music</h3>
 
-    </section>
+        <!-- Diarra -->
+         <p>In my free time I enjoy playing the piano. I have been playing classical piano since I was 4 years old, and it’s something that I’ve always enjoyed. I wouldn’t say that I’m the next Mozart or anything though! One of my favorite composers is Debussy- I really like romantic-era music. I took several music theory classes when I was in high school, but music theory doesn’t really appeal to me all that much.</p>
 
-    <section>
+      </section>
 
-      <h3>Fun facts</h3>
+      <section>
 
-      <!-- Diarra -->
-      <p>Another thing that I really enjoy is sleeping. Unfortunately I don’t get to do this as often as I would like sometimes – but I guess it is worth it in order to get an education. In the future, I’m not sure exactly what I’d like to do. Hopefully I’ll have the chance to have several different careers! I’d like to be a teacher, a researcher, a programmer, a rich person who doesn’t have to work…</p>
+        <h3>Fun facts</h3>
 
-      <!-- Priya -->
-      <p>Other interesting facts about me. Most people learn how to bike very young, but I did not learn how to ride a bike until freshman year of college. And then soon after learning how to ride a bike, I got my own bike and biked to Ontario Beach Park four times over the past summer. I like most forms of martial arts, and I participate in the karate and Tae kwon do clubs here at the university. Some other things I do for fun include singing, playing the guitar, making pencil sketches and watching movies. </p>
+        <!-- Diarra -->
+        <p>Another thing that I really enjoy is sleeping. Unfortunately I don’t get to do this as often as I would like sometimes – but I guess it is worth it in order to get an education. In the future, I’m not sure exactly what I’d like to do. Hopefully I’ll have the chance to have several different careers! I’d like to be a teacher, a researcher, a programmer, a rich person who doesn’t have to work…</p>
 
-      <!-- Hope -->
-      <p>Last semester, I was a teaching assistant for Economics 207, an intermediate microeconomics course taught by Professor Rizzo. As part of my responsiblities I graded Problem sets, exams, and help develop the course scope. I really enjoyed this course when I took it and loved the chance to teach other students. This semester I am currently working as a Teaching Assistant for the Computer Science department in an introduction to Python course, CSC 161 taught by Professor Sarkis. I really loved the problem solving aspect of the labs, and so I decided to serve as a lab TA for the course. Weekly, I run two lab sessions for upwards of 50 students and answer questions on their homework assignments. </p>
+        <!-- Priya -->
+        <p>Other interesting facts about me. Most people learn how to bike very young, but I did not learn how to ride a bike until freshman year of college. And then soon after learning how to ride a bike, I got my own bike and biked to Ontario Beach Park four times over the past summer. I like most forms of martial arts, and I participate in the karate and Tae kwon do clubs here at the university. Some other things I do for fun include singing, playing the guitar, making pencil sketches and watching movies. </p>
 
-    </section>
+        <!-- Hope -->
+        <p>Last semester, I was a teaching assistant for Economics 207, an intermediate microeconomics course taught by Professor Rizzo. As part of my responsiblities I graded Problem sets, exams, and help develop the course scope. I really enjoyed this course when I took it and loved the chance to teach other students. This semester I am currently working as a Teaching Assistant for the Computer Science department in an introduction to Python course, CSC 161 taught by Professor Sarkis. I really loved the problem solving aspect of the labs, and so I decided to serve as a lab TA for the course. Weekly, I run two lab sessions for upwards of 50 students and answer questions on their homework assignments. </p>
 
-    <?php include('./inc/scripts.php'); ?>
+      </section>
+
+    </div>
+
+    <?php include('./inc/app-scripts.php'); ?>
 
   </body>
 
--- inc/app-nav.php
+++ inc/app-nav.php
@@ -1,10 +1,6 @@
-<nav>
-    <ul>
-      <li>
-        <a href="index.php">Home</a>
-      </li>
-      <li>
-        <a href="diarra-hope-priya.php">About Them</a>
-      </li>
+<nav id="nav">
+    <ul class="ui borderless main menu">
+      <li class="header item"><a href="index.php">Home</a></li>
+      <li class="item"><a href="diarra-hope-priya.php">About Them</a></li>
     </ul>
 </nav>
