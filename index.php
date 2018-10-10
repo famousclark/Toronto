079c7d6f00059232c732a82c9aaa32c2ff0d9f31
famousclark <clark.famous@gmail.com>
2018-10-10 01:47:19 -0400

    choregraphy of file names

--- index.php
+++ index.php
@@ -16,16 +16,6 @@
 
       <h1>Odd one out!</h1>
 
-      <section>
-
-        <h2>Diarra Bell</h2>
-
-        <figure>
-          <img src="./media/diarra.jpg" alt="me" style="width:400px; height:600px;">
-        </figure>
-
-        <p>My name is Diarra Bell and I’m a junior at the University of Rochester. I am majoring in computer science with a minor in Japanese. When I first came to the U of R, I didn’t know anything about any of those subjects, but I’ve learned a lot in just two years. When I first came to Rochester, my intended major was Physics and Astronomy. I took an intro to computer science class my first semester, and I really enjoyed it. Eventually, I decided to major in computer science. I also still love astronomy, so I’d like to have a career related to that industry in the future.</p>
-      </section>
 
       <section>
 
@@ -49,6 +39,17 @@
 
         <p>Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City. I attended Solomon Schechter School of Westchester, a Jewish day school located in Hartsdale, New York. I started gymnastics when I was 7 years-old and was a competetive gymnast throughout my high school career, before deciding not to pursue it in college. Before going to college, I reached USAG level 9 gymnastics and competed on a State and Regional level. During my senior year of high school I applied Early Decision to the University of Rochester and was accepted! I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation.</p>
 
+      </section>
+
+      <section>
+
+        <h2>Diarra Bell</h2>
+
+        <figure>
+          <img src="./media/diarra.jpg" alt="me" style="width:400px; height:600px;">
+        </figure>
+
+        <p>My name is Diarra Bell and I’m a junior at the University of Rochester. I am majoring in computer science with a minor in Japanese. When I first came to the U of R, I didn’t know anything about any of those subjects, but I’ve learned a lot in just two years. When I first came to Rochester, my intended major was Physics and Astronomy. I took an intro to computer science class my first semester, and I really enjoyed it. Eventually, I decided to major in computer science. I also still love astronomy, so I’d like to have a career related to that industry in the future.</p>
       </section>
 
     </div>
--- priya-hope-diarra.php
+++ priya-hope-diarra.php
@@ -20,15 +20,15 @@
 
         <h2>About</h2>
 
-        <!-- Diarra -->
-        <p>I was born in Rochester, and I’ve lived here my whole life. I live in Brighton with my parents and my sister, who is in her senior year of high school. I enjoy living in Rochester however, when I graduate, I’d like to go somewhere new. I’m not exactly sure where I want to go to grad school yet, but I’ll figure it out… eventually. In the meantime I’d really like to visit Europe, Australia, Africa, South America… there’s so many places I haven’t been yet. I just need to find the time (and money) to do it.</p>
-
         <!-- Priya -->
         <p>My interests include music, art, design and computer science. Initially, I wasn’t sure how I might be able to combine that combination of interests, but I found that the field of mixed reality technology combined those aspects very neatly. It involved both the creative side and technical side of my interests. The environment is designed using principles of good design, and it also involves programming the software to make it work. It is a relatively new industry so there is a lot that is still being discovered and there is lots of research going on in this area, both in academia and the industry.</p>
 
         <!-- Hope -->
         <p>I have had a variety of internships and jobs throughout my life. My first job was as a gymnastics coach at Westchester Gymnastics, where I taught to beginners. I spent the summer of 2016 working as the gymnastics coordinator for a day camp run by the Jewish Community Center of Tarrytown, where I worked with over 300 children every day teaching basic gymnastics skills. In the summer of 2017 following my Freshmen year of college, I worked in the User Experience Design department of Mastercard in NYC, and got the chance to work on a variety of exciting projects such as the redesign of MC's website, apps and conduct multiple research tests. This past summer, I was a graphic design intern for Regeneron, a biotechnology company located in Tarrytown NY. In this position, I helped run and manage many of their social media campaigns, create designs for corporate materials, and learned a great deal about marketing within the heathcare industry.
   	    </p>
+
+        <!-- Diarra -->
+        <p>I was born in Rochester, and I’ve lived here my whole life. I live in Brighton with my parents and my sister, who is in her senior year of high school. I enjoy living in Rochester however, when I graduate, I’d like to go somewhere new. I’m not exactly sure where I want to go to grad school yet, but I’ll figure it out… eventually. In the meantime I’d really like to visit Europe, Australia, Africa, South America… there’s so many places I haven’t been yet. I just need to find the time (and money) to do it.</p>
 
       </section>
 
@@ -61,14 +61,14 @@
 
         <h3>Fun facts</h3>
 
-        <!-- Diarra -->
-        <p>Another thing that I really enjoy is sleeping. Unfortunately I don’t get to do this as often as I would like sometimes – but I guess it is worth it in order to get an education. In the future, I’m not sure exactly what I’d like to do. Hopefully I’ll have the chance to have several different careers! I’d like to be a teacher, a researcher, a programmer, a rich person who doesn’t have to work…</p>
-
         <!-- Priya -->
         <p>Other interesting facts about me. Most people learn how to bike very young, but I did not learn how to ride a bike until freshman year of college. And then soon after learning how to ride a bike, I got my own bike and biked to Ontario Beach Park four times over the past summer. I like most forms of martial arts, and I participate in the karate and Tae kwon do clubs here at the university. Some other things I do for fun include singing, playing the guitar, making pencil sketches and watching movies. </p>
 
         <!-- Hope -->
         <p>Last semester, I was a teaching assistant for Economics 207, an intermediate microeconomics course taught by Professor Rizzo. As part of my responsiblities I graded Problem sets, exams, and help develop the course scope. I really enjoyed this course when I took it and loved the chance to teach other students. This semester I am currently working as a Teaching Assistant for the Computer Science department in an introduction to Python course, CSC 161 taught by Professor Sarkis. I really loved the problem solving aspect of the labs, and so I decided to serve as a lab TA for the course. Weekly, I run two lab sessions for upwards of 50 students and answer questions on their homework assignments. </p>
+
+        <!-- Diarra -->
+        <p>Another thing that I really enjoy is sleeping. Unfortunately I don’t get to do this as often as I would like sometimes – but I guess it is worth it in order to get an education. In the future, I’m not sure exactly what I’d like to do. Hopefully I’ll have the chance to have several different careers! I’d like to be a teacher, a researcher, a programmer, a rich person who doesn’t have to work…</p>
 
       </section>
 
