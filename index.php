bb3252439f954831bdb598cd39c323f922ae3149
Efua <kagyarek@u.rochester.edu>
2018-10-23 16:17:46 -0400

    h

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       </div>
 
         <div class="item2">
-          <p>Hello! My name is Pooja Priya. I prefer to go by my last name Priya. I am currently a junior at the University of Rochester, majoring in Digital Media Studies and Computer Science. I grew up in Patna, India. It is a big city located near the India-Nepal border. In the Indian education system, it is common for high school students to choose from either the sciences, humanities and business streams. My high school was very focused on the sciences and as a result, I took classes in physics, chemistry and biology. In high school, computer science used to be my favorite subject and so I chose that as my major when I got to college. After my first semester, I realized that the University of Rochester curriculum was a lot more flexible in terms of which classes I could take. I took a few music and design classes to explore my interests and realized that I really liked studying what makes good design, and I ended up adding Digital Media Studies as a second major.</p>
+          <p>Hello! My name is Pooja Priya. I prefer to go by my last name Priya. I am currently a junior at the University of Rochester, majoring in Digital Media Studies and Computer Science. I grew up in Patna, India. In the Indian education system, it is common for high school students to choose from either the sciences, humanities and business streams. My high school was very focused on the sciences and as a result, I took classes in physics, chemistry and biology. In high school, computer science used to be my favorite subject and so I chose that as my major when I got to college. After my first semester, I realized that the University of Rochester curriculum was a lot more flexible in terms of which classes I could take. I took a few music and design classes to explore my interests and realized that I really liked studying what makes good design, and I ended up adding Digital Media Studies as a second major.</p>
         </div>
 
        <div class="item3"> 
@@ -35,7 +35,7 @@
           </figure> </div>
 
         <div class="item4">
-         <p>Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City. I attended Solomon Schechter School of Westchester, a Jewish day school located in Hartsdale, New York. I started gymnastics when I was 7 years-old and was a competetive gymnast throughout my high school career, before deciding not to pursue it in college. Before going to college, I reached USAG level 9 gymnastics and competed on a State and Regional level. During my senior year of high school I applied Early Decision to the University of Rochester and was accepted! I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation.</p>
+         <p>Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City. I started gymnastics when I was 7 years-old and was a competetive gymnast throughout my high school career, before deciding not to pursue it in college. During my senior year of high school I applied Early Decision to the University of Rochester and was accepted! I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation.</p>
         </div>
 
         <div class="item5">
@@ -50,6 +50,8 @@
           <p>My name is Diarra Bell and I’m a junior at the University of Rochester. I am majoring in computer science with a minor in Japanese. When I first came to the U of R, I didn’t know anything about any of those subjects, but I’ve learned a lot in just two years. When I first came to Rochester, my intended major was Physics and Astronomy. I took an intro to computer science class my first semester, and I really enjoyed it. Eventually, I decided to major in computer science. I also still love astronomy, so I’d like to have a career related to that industry in the future.</p>
         </div>
 
+         <a class="item16" href="priya-hope-diarra.php">Click here to learn more!</a>
+       
     </div>
 
     <?php include('./inc/app-scripts.php'); ?>
